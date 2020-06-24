75cf212d013453f83b26f7cc8d34f6985fb5a77d
Martin <martin@dreamweb.space>
2020-06-24 11:34:50 +0100

    main game structure

---
+++ game.js
@@ -0,0 +1,18 @@
+const textElement = document.getElementById('text');
+const optionButtonElement = document.getElementById('option-buttons');
+let state = {};
+
+function startGame() {
+
+};
+
+function showTextNode(TextNodeIndex) {
+
+};
+
+function selectOption(option) {
+
+};
+
+startGame();
+
